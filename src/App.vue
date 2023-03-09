bfcc161b9d365154c8c7eb76afd17ca5c5f0f7a8
Tiago Matos <tiago@tiagomatos.com>
2023-03-10 09:51:19 +1100

    Aula 02

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <v-layout>
+      <v-app-bar flat density="comfortable" class="text-center border-b">
+        <v-app-bar-title>Vuetify cart</v-app-bar-title>
+
+        <template #append>
+          <v-badge color="info" dot>
+            <v-icon icon="mdi-cart" />
+          </v-badge>
+        </template>
+      </v-app-bar>
+
+      <v-main>
+        <HelloWorld />
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
