7b4d3cffd03de6b9d0c854b0ca44716e58a2f44f
Tiago Matos <tiago@tiagomatos.com>
2023-03-10 11:32:47 +1100

    Aula 03

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app theme="dark">
     <v-layout>
       <v-app-bar flat density="comfortable" class="text-center border-b">
         <v-app-bar-title>Vuetify cart</v-app-bar-title>
@@ -11,13 +11,174 @@
         </template>
       </v-app-bar>
 
-      <v-main>
-        <HelloWorld />
+      <v-main class="mt-6">
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="4" lg="3">
+              <v-card flat class="border">
+                <div class="bg-white px-6 py-2">
+                  <v-img src="https://images.kabum.com.br/produtos/fotos/99866/monitor-lg-led-23-8-widescreen-full-hd-ips-hdmi-24mk430h_1547830365_m.jpg" />
+                </div>
+
+                <v-card-text>
+                  <h4>Monitor LG 23.8' IPS, Full HD, HDMI, VESA, Ajuste de Ângulo - 24MK430H</h4>
+
+                  <div class="my-4">
+                    <div class="font-weight-light text-decoration-line-through mb-n2">R$ 965,90</div>
+                    <div class="text-h5 font-weight-bold">R$ 699,99</div>
+                    <div class="font-weight-light">À vista no PIX</div>
+                  </div>
+
+                  <v-btn flat block color="primary">Adicionar</v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-main>
     </v-layout>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
 </script>
